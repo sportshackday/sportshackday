1e930264239dd81ebc6d776bbd8516e2dff99702
Alex Lande <alexlande@gmail.com>
2013-01-02 15:04:56 -0800

    Add Twitter Feed and Tickets button.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-(function(){})();
+$(document).ready(function () {
+
+function getTweets() {
+  $(".tweets").miniTwitter({
+    query: "#sportshackday OR @sportshackday OR sportshackday.com",
+    limit: 10
+  });
+}
+
+// populate tweets imperatively
+getTweets();
+
+// timer to update tweets every 10 seconds
+setInterval(function(){ getTweets(); }, 10000);
+});
